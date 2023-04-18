4feb166be8009d337a161b4b215530c8c282ce2f
Georgi <112810653+Gogo8618@users.noreply.github.com>
2023-04-18 18:40:28 +0300

    OOP

---
+++ wild cat zoo/keeper.py
@@ -0,0 +1,8 @@
+from library.worker import Worker
+
+
+class Keeper(Worker):
+
+    def __init__(self, name: str, age: int, salary: int):
+        super().__init__(name, age, salary)
+
---
+++ wild cat zoo/zoo.py
@@ -0,0 +1,29 @@
+from library.animal import Animal
+from library.worker import Worker
+
+
+class Zoo:
+
+    def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+
+        self.animals = []
+        self.workers = []
+
+    def add_animal(self, animal: Animal, price: int):
+        if self.__budget - price < 0:
+            return "Not enough budget"
+        if len(self.animals) == self.__animal_capacity:
+            return "Not enough space for animal"
+        self.animals.append(animal)
+        self.__budget -= price
+        return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+
+    def hire_worker(self, worker: Worker):
+        if len(self.workers) == self.__workers_capacity:
+            return "Not enough space for worker"
+        self.workers.append(worker)
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
